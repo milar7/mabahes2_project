f22a975f0db0a44d5d28fc2d553f8e56dc396f7e
milad nafar <milarnafar78+glab@gmail.com>
2021-05-15 19:49:24 +0430

    add pages folder and initial home,login,register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
+import {
+  BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import './App.css';
+import Home from "./pages/home/home";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">register</Link>
+            </li>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        <Route exact path="/" >
+            <Home />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+         
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
